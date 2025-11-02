ef03ae7014424189d70f4390f5273c192ab3909c
karthika <karthik@gmail.com>
2025-11-02 18:09:15 +0530

    added index file

---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div style={{ textAlign: "center", marginTop: "100px" }}>
+        <h1>Digital Wellbeing & Productivity Timer</h1>
+        <p>Stay focused, stay balanced.</p>
+      </div>
+    );
+  }
+  
